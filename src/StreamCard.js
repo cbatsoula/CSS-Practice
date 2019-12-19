ef15d5e44f71633cc9170a3822fb34aee3c6c271
cbatsoula <cbatsoula@gmail.com>
2019-12-19 13:18:18 -0500

    working

--- src/StreamCard.js
+++ src/StreamCard.js
@@ -4,11 +4,10 @@ import './App.css';
 class StreamCard extends React.Component {
 
   render () {
-    console.log("what", this.props.el)
+    // console.log("what", this.props)
     return (
       <div className="StreamCard">
         <h3> Streamin </h3>
-        <h3> {this.props.key} </h3>
       </div>
     );
 
