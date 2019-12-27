b640bd7120a3897caee436ffbe7f8f6cf8fdd910
cbatsoula <cbatsoula@gmail.com>
2019-12-27 15:07:42 -0500

    added streamDetails under StreamCard

---
+++ src/StreamDetail.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+class StreamDetail extends React.Component {
+
+  render () {
+    // console.log("what", this.props)
+    return (
+      <div className="StreamDetailCard">
+        <p> Stream Title </p>
+        <p> User </p>
+
+      </div>
+    );
+
+  }
+}
+
+export default StreamDetail;
