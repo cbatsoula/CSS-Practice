fcfe65ab93ebe0f7a48e2c8f6d62e2cc6f97b5d3
cbatsoula <cbatsoula@gmail.com>
2019-12-27 14:56:06 -0500

    adding nav buttons

--- src/Nav.js
+++ src/Nav.js
@@ -5,6 +5,11 @@ class Nav extends React.Component {
   render () {
     return (
       <div className="Nav">
+        <button>Home</button>
+        <button>Explore</button>
+        <button>Channels</button>
+        <button>Sign up</button>
+        <button>Log in</button>
 
       </div>
     );
